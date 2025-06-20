2f73b0e2063f467ddd4cc2aa91296f763331db01
ronel-meli <ronelmeli24@gmail.com>
2025-06-20 16:27:58 +0300

    first commmit for selection sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,21 @@
+arr=[34,7,90,76,34]
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+
+    for i in range(n - 1):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        if min_index != i:
+
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+print(selection_sort(arr))
+
+
+
+
