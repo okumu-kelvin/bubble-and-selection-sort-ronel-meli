57cfdfe64fae99906c6e79fd98e551b28672010d
ronel-meli <ronelmeli24@gmail.com>
2025-06-20 10:19:49 +0300

    first commmit for bubble sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,18 @@
+unsorted_list=[12,9,67,45,90]
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+   n= len(unsorted_list)
+   for i in range(n-1):
+        for j in range(n-1-i):
+            if unsorted_list[j]>unsorted_list[j+1]:
+                unsorted_list[j],unsorted_list[j+1]=unsorted_list[j+1],unsorted_list[j]
+
+   return unsorted_list
+
+print(bubble_sort(unsorted_list))
+
+
+
+
+
+
+
